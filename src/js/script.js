909402ad0a50c1ea1404dfbfee80933a472161de
Ageng Ari <agengarinugroho@gmail.com>
2023-03-13 12:25:01 +0700

    Navbar Selesai
    Menyelesaikan responsive navbar, ditambah sedikit membenahi high konten welcome screen menjadi 100vh.

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.querySelector(".navbar-burger");
-const navLinks = document.querySelector("#menuMobile");
+const navLinks = document.querySelector(".menu-mobile");
 
 toggleButton.addEventListener("click", () => {
   navLinks.classList.toggle("hidden");
